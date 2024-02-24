f1dadd6898aeec46089af2b3e1a73172393f54a6
Aaron Decker <decker49@gmail.com>
2024-02-23 23:49:59 -0600

    Start building out nouns.

--- src/whitakers_clj/core.clj
+++ src/whitakers_clj/core.clj
@@ -4,10 +4,50 @@
   {"N" :noun
    "V" :verb})
 
+(def grammatical-case
+  {"NOM" :nominative
+   "GEN" :genitive
+   "DAT" :dative
+   "ACC" :accusative
+   "ABL" :ablative
+   "VOC" :vocative})
+
+(def grammatical-number
+  {"S" :singular
+   "P" :plural})
+
+(def gender
+  {"M" :masculine
+   "F" :feminine
+   "N" :neuter})
+
+(defn add-verb-pieces [pieces]
+  {:sectioned-word (get-in pieces [0 0])
+   :part-of-speech (part-of-speech (get-in pieces [0 1]))
+   :conjugation (parse-long (get-in pieces [0 2]))})
+
+(defn add-noun-pieces [pieces]
+  (let [sectioned-word (get-in pieces [0 0])
+        [stem ending] (clojure.string/split sectioned-word #"\.")]
+    {:sectioned-word sectioned-word
+     :stem stem
+     :ending ending
+     :part-of-speech (part-of-speech (get-in pieces [0 1]))
+     :declension (parse-long (get-in pieces [0 2]))
+     ;; :variant (parse-long (get-in pieces [0 3])) ;; not sure this means anything grammatical
+     :case (grammatical-case (get-in pieces [0 4]))
+     :number (grammatical-number (get-in pieces [0 5]))
+     :gender (gender (get-in pieces [0 6]))
+     :dictionary-entry (str (get-in pieces [1 0]) " " (get-in pieces [1 1]))
+     :definition (clojure.string/join " " (get pieces 2))}))
+
+(def parse-by-part-of-speech
+  {:verb add-verb-pieces
+   :noun add-noun-pieces})
+
 (defn parse-single-word-output [paragraph]
   (let [lines (clojure.string/split-lines paragraph)
-        pieces (mapv #(clojure.string/split % #" +") lines)]
-    {:sectioned-word (get-in pieces [0 0])
-     :part-of-speech (part-of-speech (get-in pieces [0 1]))
-     :declension (parse-long (get-in pieces [0 2]))}))
+        pieces (mapv #(clojure.string/split % #" +") lines)
+        part-of-speech (part-of-speech (get-in pieces [0 1]))]
+    ((parse-by-part-of-speech part-of-speech) pieces)))
 
--- test/whitakers_clj/core_test.clj
+++ test/whitakers_clj/core_test.clj
@@ -7,8 +7,29 @@
 amo, amare, amavi, amatus  V (1st)   [XXXAO]  
 love, like; fall in love with; be fond of; have a tendency to;")
 
+(def agricolarum-paragraph
+"agricol.arum         N      1 1 GEN P M                 
+agricola, agricolae  N (1st) M   [XAXBO]  
+farmer, cultivator, gardener, agriculturist; plowman, countryman, peasant;
+agricolar.um         ADJ    3 2 GEN P X POS             
+agricolaris, agricolaris, agricolare  ADJ   [XAXES]    uncommon
+farmer-; relating to farmers;")
+
 (deftest parse-single-word-output-test
-  (let [parsed-obj (parse-single-word-output amo-paragraph)]
-    (is (= (:sectioned-word parsed-obj) "am.o"))
-    (is (= (:part-of-speech parsed-obj) :verb))
-    (is (= (:declension parsed-obj) 1))))
+  (testing "amo"
+    (let [parsed-obj (parse-single-word-output amo-paragraph)]
+      (is (= (:sectioned-word parsed-obj) "am.o"))
+      (is (= (:part-of-speech parsed-obj) :verb))
+      (is (= (:conjugation parsed-obj) 1))))
+  (testing "agricolarum"
+    (let [parsed-obj (parse-single-word-output agricolarum-paragraph)]
+      (is (= (:stem parsed-obj) "agricol"))
+      (is (= (:ending parsed-obj) "arum"))
+      (is (= (:part-of-speech parsed-obj) :noun))
+      (is (= (:declension parsed-obj) 1))
+      (is (= (:case parsed-obj) :genitive))
+      (is (= (:number parsed-obj) :plural))
+      (is (= (:gender parsed-obj) :masculine))
+      (is (= (:dictionary-entry parsed-obj) "agricola, agricolae"))
+      (is (= (:definition parsed-obj) "farmer, cultivator, gardener, agriculturist; plowman, countryman, peasant;"))
+      )))
