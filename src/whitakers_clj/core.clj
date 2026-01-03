fa8df3c6604281a991295444193f7d27251af78a
Aaron Decker <decker49@gmail.com>
2026-01-03 01:02:24 -0600

    Fix a parsing crash on dissēparābat.

--- src/whitakers_clj/core.clj
+++ src/whitakers_clj/core.clj
@@ -407,6 +407,7 @@
            lines (remove #(re-matches #"ex                   PREFIX.*" %) lines)
            lines (remove #(re-matches #"- out, away from; beyond; completely;.*" %) lines)
            lines (remove #(re-matches #"di                   PREFIX.*" %) lines)
+           lines (remove #(re-matches #"dis                  PREFIX.*" %) lines)
            lines (remove #(re-matches #"- apart/asunder, in.*" %) lines)
            lines (remove #(re-matches #"PACKON.*" %) lines)
            lines (remove #(re-matches #"- together; completely.*" %) lines)
